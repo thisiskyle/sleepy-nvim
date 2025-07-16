9eb2a7d9848ef898a6edd7b62652a755d393e153
Kyle Ericson <kyle.ericson@gentex.com>
2025-07-16 14:59:25 -0400

    refactor how we handle the data section of curl building
    * check data type (string or table) instead of assuming
    * can now accept a lua object as body and convert to json
    * strings still work, but using the [[ ]] thing is still weird

--- lua/sleepy/curl.lua
+++ lua/sleepy/curl.lua
@@ -1,6 +1,8 @@
 ---@class sleepy.RequestData
 ---@field urlencode? string
 ---@field raw? string
+---@field text? string
+---@field obj? table
 
 ---@class sleepy.HttpRequest table with the data needed to make an http request
 ---@field type string
@@ -19,6 +21,33 @@ local request_types = {
     get = { "-X", "GET", "--get" },
     post = { "-X", "POST" },
 }
+
+local function data_urlencode(cmd_table, data)
+    if(type(data) == "string") then
+        table.insert(cmd_table, "--data-urlencode")
+        table.insert(cmd_table, data)
+    end
+end
+
+local function data_raw(cmd_table, data)
+    if(type(data) == "string") then
+        table.insert(cmd_table, "--data-raw")
+        table.insert(cmd_table, data)
+    end
+end
+
+local function data_standard(cmd_table, data)
+    if(type(data) == "string") then
+        table.insert(cmd_table, "--data")
+        table.insert(cmd_table, data)
+
+    elseif(type(data) == "table") then
+        table.insert(cmd_table, "--data")
+        table.insert(cmd_table, vim.json.encode(data))
+    end
+end
+
+
 
 --- Build the curl command string from a sleepy.HttpRequest
 ---@param request sleepy.HttpRequest
@@ -52,17 +81,17 @@ function M.build(request)
     if(request.data) then
         for _,v in ipairs(request.data) do
             if(v.urlencode) then
-                table.insert(curl_command, "--data-urlencode")
-                table.insert(curl_command, v.urlencode)
+                data_urlencode(curl_command, v.urlencode)
             elseif(v.raw) then
-                table.insert(curl_command, "--data")
-                table.insert(curl_command, v.raw)
+                data_raw(curl_command, v.raw)
+            elseif(v.text) then
+                data_standard(curl_command, v.text)
+            elseif(v.obj) then
+                data_standard(curl_command, v.obj)
             elseif(type == "get") then
-                table.insert(curl_command, "--data-urlencode")
-                table.insert(curl_command, v[1])
+                data_urlencode(curl_command, v[1])
             else
-                table.insert(curl_command, "--data")
-                table.insert(curl_command, v[1])
+                data_standard(curl_command, v[1])
             end
         end
     end
