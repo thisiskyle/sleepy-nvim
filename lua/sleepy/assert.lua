88fba8ffecce8b95f22c721f75bd2889ff233136
Kyle Ericson <kyle.ericson@gentex.com>
2025-07-16 14:58:42 -0400

    use vim.json.decode over vim.fn.json_decode

--- lua/sleepy/assert.lua
+++ lua/sleepy/assert.lua
@@ -1,7 +1,9 @@
 local M = {}
 
---- Assumes a json string is provided. Checks it for a key value pair.
----@param data string
+--- Assumes a json string is provided as an array of strings. 
+--- This is because of the way the data is brought back in chunks
+--- Checks it for a key value pair.
+---@param data string[]
 ---@param path string[]
 ---@param value string
 ---@return boolean
@@ -10,13 +12,20 @@ function M.json_path_value(data, path, value)
     if(not data) then
         return false
     end
-    local json_tbl = vim.fn.json_decode(data)
+
+    local data_str = ""
+    for _,v in ipairs(data) do
+        data_str = data_str .. v
+    end
+
+    local json_tbl = vim.json.decode(data_str)
     local current = json_tbl
     for _,k in pairs(path) do
         if(type(current) == "table") then
             current = current[k]
         end
     end
+
     if(current == value) then
         return true
     end
@@ -24,7 +33,7 @@ function M.json_path_value(data, path, value)
 end
 
 --- Assumes a json string is provided. Checks it for a key.
----@param data string
+---@param data string[]
 ---@param path string[]
 ---@return boolean
 ---
@@ -32,7 +41,13 @@ function M.json_has_key(data, path)
     if(not data) then
         return false
     end
-    local json_tbl = vim.fn.json_decode(data)
+
+    local data_str = ""
+    for _,v in ipairs(data) do
+        data_str = data_str .. v
+    end
+
+    local json_tbl = vim.json.decode(data_str)
     local current = json_tbl
     for _,k in pairs(path) do
         if(type(current) == "table") then
